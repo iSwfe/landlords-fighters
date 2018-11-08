5c376c33fc70da454c4a54fad33ca62df5b5f964
iSwfe <iSwfe@qq.com>
2018-11-09 01:57:11 +0800

    project init but not .gitignore.

---
+++ src/main/java/com/iswfe/games/landlords_fighters/LandlordsFightersApplication.java
@@ -0,0 +1,12 @@
+package com.iswfe.games.landlords_fighters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordsFightersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandlordsFightersApplication.class, args);
+    }
+}
---
+++ src/test/java/com/iswfe/games/landlords_fighters/LandlordsFightersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iswfe.games.landlords_fighters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LandlordsFightersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
